ec4a3bf43957f3deb38fbfca4e8d5a5021479b1f
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-11-12 23:38:23 -0300

    se agregaron test

--- Clase-8/src/utils/index.js
+++ Clase-8/src/utils/index.js
@@ -13,3 +13,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default __dirname;
+
+
+export const suma = (num1,num2) => {
+    if(typeof num1 !== 'number' || typeof num2 !== 'number') return 0
+    return num1 + num2
+} 
