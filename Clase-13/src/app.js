46c69a6c71d3b3c8d429fa14ef7e8a8f57e41322
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-11-28 19:36:22 -0300

    se agrega mongo url

---
+++ Clase-13/src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+
+import usersRouter from './routes/users.router.js';
+import petsRouter from './routes/pets.router.js';
+import adoptionsRouter from './routes/adoption.router.js';
+import sessionsRouter from './routes/sessions.router.js';
+
+const app = express();
+const PORT = process.env.PORT||8080;
+const connection = mongoose.connect(`mongodb+srv://danielvillajuan:qpCcWENKy5dp6gRi@coderback.dkldvkl.mongodb.net/?retryWrites=true&w=majority&appName=Coderback`)
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use('/api/users',usersRouter);
+app.use('/api/pets',petsRouter);
+app.use('/api/adoptions',adoptionsRouter);
+app.use('/api/sessions',sessionsRouter);
+
+app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
