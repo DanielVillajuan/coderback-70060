def3ebed3fd53212f3f223852d1556cabed9e3b0
Daniel Villajuan <danielvillajuan@hotmail.com>
2024-11-28 21:05:45 -0300

    clase 13 - final

--- Clase-13/src/controllers/users.controller.js
+++ Clase-13/src/controllers/users.controller.js
@@ -8,7 +8,9 @@ const getAllUsers = async(req,res)=>{
 const getUser = async(req,res)=> {
     const userId = req.params.uid;
     const user = await usersService.getUserById(userId);
-    if(!user) return res.status(404).send({status:"error",error:"User not found"})
+    if(!user){
+        res.status(404).send({status:"error",error:"User not found"})
+    }  
     res.send({status:"success",payload:user})
 }
 
